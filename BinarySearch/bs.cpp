a7dbbc971eb81b9a3020288c659146ce3e6f1f6c
nickonfix <rjnikhil45@gmail.com>
2025-11-18 21:05:22 +0530

    BinarySearch

--- BinarySearch/bs.cpp
+++ BinarySearch/bs.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
+#include <iostream> 
+
 using namespace std;
 
-int main() {
+int main(){
+    
     int arr[] = {1,2,3,4,5,6,7,8,9,10};
     int size = sizeof(arr)/sizeof(arr[0]);
     int key = 7;
@@ -13,6 +15,5 @@ int main() {
             cout<<"Element found at index: "<<mid<<endl;
         }
     }
-    return 0;
-
+    return 0;       
 }
