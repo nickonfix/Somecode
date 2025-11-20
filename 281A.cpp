5e32f6e5c977400f26ecab0286ab7d1c2ef77af5
nickonfix <rjnikhil45@gmail.com>
2025-11-20 21:00:02 +0530

    Consistency is the key

---
+++ 281A.cpp
@@ -0,0 +1,17 @@
+//Word Capitalization
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main () {
+
+    string s;
+    cin>> s;
+    int i=0;
+
+    s[i] = toupper(s[i]);
+    cout  << s;
+
+}
+
